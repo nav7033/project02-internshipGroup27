009d7748f50fbc2bfdb1bb5a12f18c35fd53032c
nav7033 <mandaln51@gmail.com>
2022-03-21 17:55:23 +0530

    project02

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const { boolean } = require('webidl-conversions')
 
 const collegeSchema = new mongoose.Schema({
     name:{
@@ -19,7 +18,7 @@ const collegeSchema = new mongoose.Schema({
 
     },
     isDeleted:{
-        type:boolean,
+        type:Boolean,
         default:false
     }
 
